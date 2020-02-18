21ce9bded7243507a8c0d8ed781ca2ade9f29046
okanisildur <okanisildar@gmail.com>
2020-02-18 18:57:20 +0300

    fix: load more news is being observed, too

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 const targetNode = document.getElementsByClassName("news");
-const config = { childList: true };
+const config = { subtree: true, childList: true };
 
 const callback = function(mutationsList, observer) {
   for (const mutation of mutationsList) {
@@ -7,9 +7,6 @@ const callback = function(mutationsList, observer) {
       const collapsedElements = document.getElementsByClassName("Details js-news-feed-event-group");
       for (let i = 0; i < collapsedElements.length; i++) {
         collapsedElements[i].className += "open Details--on";
-      }
-      if (collapsedElements.className === "Details js-news-feed-event-group open Details--on") {
-        observer.disconnect();
       }
     }
   }
